d6db6ef8523721a54b13d1b98d137ad06a1e0590
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-02-11 13:27:44 +0100

    create and include header

--- fullstack/news_list.php
+++ fullstack/news_list.php
@@ -3,16 +3,12 @@
 
 <head>
     <style>
-        .d-flex {
+        .news-list {
             gap: 2rem;
         }
     </style>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<title>List</title>
+    <?php include "header.php"; ?>
 </head>
 
 <body>
@@ -24,10 +20,10 @@
         </div>
         <div class="row">
             <div class="col-12">
-                <div class="d-flex flex-wrap">
+                <div class="d-flex flex-wrap news-list">
 
                     <?php
-                    include_once "class_news.php";
+                    require_once "class_news.php";
                     foreach ($news_db as $elem) :
                     ?>
                         <div class="card" style="width: 18rem;">
--- fullstack/news_single.php
+++ fullstack/news_single.php
@@ -2,11 +2,8 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Single</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<?php include "header.php"; ?>
 </head>
 
 <body>
