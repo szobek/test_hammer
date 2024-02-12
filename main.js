ff59014228566010d8dc2caa43589a59f9eda364
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-02-12 19:21:13 +0100

    fix error in main,change plain alert to bs alert

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $("#btn-submit").click(function(e) {
       e.preventDefault();
+      $("#alerts").show()
       $.ajax({
         type: "POST",
         url: "control.php",
@@ -11,16 +12,28 @@ $(document).ready(function() {
         success: function(result) {
             const res = JSON.parse(result)
             if(!res.success){
-                alert(`ok de hibás adat| name: ${res.errors.name}`);
+              const alertText = '<div class="alert alert-success alert-dismissible fade show" role="alert" id="successalert">   <p id="msgs"></p>              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>'
+              $("#alertdiv").append(alertText)
+              const msg=`ok de hibás adat| name: ${res.errors.name}`
+              $('#msgs').text(msg)
             }else{
-                alert(`${res.message}`)
+              const alertText = '<div class="alert alert-success alert-dismissible fade show" role="alert" id="successalert">   <p id="msgs"></p>              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>'
+              $("#alertdiv").text(alertText)
+              const msg =`${res.message}`
+              $('#msgs').append(msg)
             }
           
           
         },
         error: function(result) {
-          alert('error');
+          const alertText ='<div class="alert alert-danger alert-dismissible fade show" role="alert" id="dangeralert">          <p id="msgd"></p>          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>        </div>'
+          $("#alertdiv").append(alertText)
+          $("#msgd").text("error")
         }
       });
     });
+
+    $(".btn-close").click(()=>{
+      $("#alerts").hide()
+    })
 })
