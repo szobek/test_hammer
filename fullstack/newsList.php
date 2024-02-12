d1a1c477ba553c49e9fb5b3463dde249f3adc1a3
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-02-12 23:00:54 +0100

    modify title on list

--- fullstack/newsList.php
+++ fullstack/newsList.php
@@ -16,7 +16,7 @@
     <div class="container ">
         <div class="row">
             <div class="col-12">
-                <h1 class="m-5 text-center"> Hírek</h1>
+                <h1 class="m-5 text-center"> News</h1>
             </div>
         </div>
         <div class="row">
