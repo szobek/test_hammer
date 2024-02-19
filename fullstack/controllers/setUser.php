56313cbddb4b3a68e1133f76d638eaf9d1c664af
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-02-19 20:59:33 +0100

    setUser class

--- fullstack/controllers/setUser.php
+++ fullstack/controllers/setUser.php
@@ -1,32 +1,45 @@
 <?php
-session_start();
-switch ($_REQUEST["function"]) {
-    case "logout":
-        logout();
-        break;
-      case "checklogin":
-checkLogin();
-break;
-    default:
-        null;
-}
 
-function logout(){
-    require_once "usercontroller.php";
-    loggingout();
-}
-function checkLogin(){
+class User
+{
+    private String $url;
 
-    $req = $_REQUEST;
-    require_once "../connectToDb.php";
+    public function __construct()
+    {
+        session_start();
+        $this->url=$_REQUEST["function"];
 
-    $result = open("SELECT * FROM `users` WHERE `email`=? and `password`=?",[$req["username"], sha1($req["psw"])]);
-    if(count($result)>0){
-        $_SESSION["logged"]=true;
-        $_SESSION["id"]=$result[0]["id"];
-header('Location: /views/admin.php');
+        switch ($this->url) {
+            case "logout":
+                $this->logout();
+                break;
+            case "checklogin":
+                $this->checkLogin();
+                break;
+            default:
+                null;
+        }
     }
-    else  header('Location: /views/login.php' , true);
 
+
+    function logout()
+    {
+        require_once "usercontroller.php";
+        loggingout();
+    }
+    function checkLogin()
+    {
+
+        $req = $_REQUEST;
+        require_once "../connectToDb.php";
+
+        $result = open("SELECT * FROM `users` WHERE `email`=? and `password`=?", [$req["username"], sha1($req["psw"])]);
+        if (count($result) > 0) {
+            $_SESSION["logged"] = true;
+            $_SESSION["id"] = $result[0]["id"];
+            header('Location: /views/admin.php');
+        } else  header('Location: /views/login.php', true);
+    }
 }
 
+new User();
